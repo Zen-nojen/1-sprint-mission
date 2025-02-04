3bf9eaac749fc30b06d9a96bee16b7d8d7a0ae35
June-Park-developer <jungeunp97@naver.com>
2025-02-04 20:25:28 +0900

    createProduct 함수 생성, 정상작동 확인

--- productService.js
+++ productService.js
@@ -4,6 +4,7 @@ const instance = axios.create({
   baseURL: "https://panda-market-api-crud.vercel.app/products",
 });
 
+// getProductList : 해당 조건 get 하여 list로 반환
 async function getProductList(page, pageSize, keyword) {
   let res;
   try {
@@ -20,6 +21,7 @@ async function getProductList(page, pageSize, keyword) {
   return res.data.list;
 }
 
+// getProduct : 해당 id의 data 반환
 async function getProduct(id) {
   let res;
   try {
@@ -28,4 +30,26 @@ async function getProduct(id) {
     console.log(error);
   }
   return res.data;
+}
+
+// createProduct : 포스트 후 id 반환
+async function createProduct(images, tags, price, description, name) {
+  let res;
+  try {
+    res = await instance.post("", {
+      images,
+      tags,
+      price,
+      description,
+      name,
+    });
+
+    if (res.status < 200 || res.status > 300) {
+      throw new Error("res 문제.");
+    }
+  } catch (error) {
+    console.error("포스팅에 문제 발생");
+    console.log(error);
+  }
+  return res.data.id;
 }
