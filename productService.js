a43bb02b616073178cdb6558393c770f7d3da15b
June-Park-developer <jungeunp97@naver.com>
2025-02-04 18:13:25 +0900

    getProduct 함수 생성, 정상작동 확인

--- productService.js
+++ productService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://panda-market-api-crud.vercel.app",
+  baseURL: "https://panda-market-api-crud.vercel.app/products",
 });
 
 async function getProductList(page, pageSize, keyword) {
   let res;
   try {
-    res = await instance.get("/products", {
+    res = await instance.get("", {
       params: {
         page,
         pageSize,
@@ -18,4 +18,14 @@ async function getProductList(page, pageSize, keyword) {
     console.error(error);
   }
   return res.data.list;
+}
+
+async function getProduct(id) {
+  let res;
+  try {
+    res = await instance.get(`/${id}`);
+  } catch (error) {
+    console.log(error);
+  }
+  return res.data;
 }
