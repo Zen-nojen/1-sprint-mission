ceef305389f9acdab06739a424826d265e7c9286
Gyeon <tjrrl1623@naver.com>
2025-02-05 11:28:32 +0900

    article.js 입력

--- sMission1/article.js
+++ sMission1/article.js
@@ -1,23 +1,23 @@
 class Article {
-	  constructor(title, content, writer = "", likeCount = 0) {
-		      this.title = title;
-		      this.content = content;
-		      this.writer = writer;
-		      this.likeCount = likeCount;
-		      this.createdAt = new Date();
-		    }
+	constructor(title, content, writer = "", likeCount = 0) {
+		this.title = title;
+		this.content = content;
+		this.writer = writer;
+		this.likeCount = likeCount;
+		this.createdAt = new Date();
+		}
 
-	  set likeCount(count) {
-		      throw new Error("call like()");
-		    }
+	set likeCount(count) {
+		throw new Error("call like()");
+		}
 
-	  get likeCount() {
-		      return this._likeCount;
-		    }
+	get likeCount() {
+		return this._likeCount;
+		}
 
-	  like() {
-		      this.likeCount += 1;
-		    }
+	like() {
+		this.likeCount += 1;
+		}
 }
 
 export { Article };
