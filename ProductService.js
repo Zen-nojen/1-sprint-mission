3733db48fa1e0603b39d9dfbb593dc7f6bf00a92
funfun <alpha5179@naver.com>
2025-02-05 11:49:45 +0900

    feat: Product API 함수 구현

--- ProductService.js
+++ ProductService.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 //Product 클래스
 export class Product {
     constructor({ name, description, price, tags, images, favoriteCount }) {
@@ -26,3 +28,94 @@ const instance = axios.create({
     baseURL: 'https://panda-market-api-crud.vercel.app',
     timeout: 10000,
 });
+
+//상품 리스트 가져오기 메소드
+export const getProductList = async (queryParams = { page: 1, pageSize: 10, keyword: '' }) => {
+    try {
+        const res = await instance.get(`/products?`, { params: queryParams });
+        return res;
+    } catch (e) {
+        if (e.response) {
+            console.log(e.response.status);
+            console.log(e.response.data);
+            throw new Error('가져오는 중 오류 발생');
+        } else if (e.request) {
+            console.log('리퀘스트는 전송되었지만 응답이 오지 않은 오류');
+        } else {
+            console.log('그 외 오류 발생');
+        }
+    }
+};
+
+//특정 상품 가져오기 메소드
+export const getProduct = async (id) => {
+    try {
+        const res = await instance.get(`/products/${id}`);
+        return res;
+    } catch (e) {
+        if (e.response) {
+            console.log(e.response.status);
+            console.log(e.response.data);
+            throw new Error('가져오는 중 오류 발생');
+        } else if (e.request) {
+            console.log('리퀘스트는 전송되었지만 응답이 오지 않은 오류');
+        } else {
+            console.log('그 외 오류 발생');
+        }
+    }
+};
+
+//상품 만들기 메소드
+export const createProduct = async (images, tags, price, description, name) => {
+    try {
+        const productData = { images, tags, price, description, name };
+        const res = await instance.post('/products', productData);
+        return res;
+    } catch (e) {
+        if (e.response) {
+            console.log(e.response.status);
+            console.log(e.response.data);
+            throw new Error('가져오는 중 오류 발생');
+        } else if (e.request) {
+            console.log('리퀘스트는 전송되었지만 응답이 오지 않은 오류');
+        } else {
+            console.log('그 외 오류 발생');
+        }
+    }
+};
+
+//상품 업데이트 메소드
+export const patchProduct = async (id, updatedContent) => {
+    try {
+        const res = await instance.patch(`/products/${id}`, updatedContent);
+        return res;
+    } catch (e) {
+        if (e.response) {
+            console.log(e.response.status);
+            console.log(e.response.data);
+            throw new Error('상품 업데이트 실패');
+        } else if (e.request) {
+            console.log('리퀘스트는 전송되었지만 응답이 오지 않은 오류');
+        } else {
+            console.log('그 외 오류 발생');
+        }
+    }
+};
+
+//상품 삭제하기 메소드
+export const deleteProduct = async (id) => {
+    try {
+        const res = await instance.delete(`/products/${id}`);
+        return res;
+    } catch (e) {
+        if (e.response) {
+            console.log(e.response.status);
+            console.log(e.response.data);
+            throw new Error('상품 삭제 실패');
+        } else if (e.request) {
+            console.log('리퀘스트는 전송되었지만 응답이 오지 않은 오류');
+        } else {
+            console.log('그 외 오류 발생');
+        }
+    }
+};
