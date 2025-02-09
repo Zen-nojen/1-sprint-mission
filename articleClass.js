e8321d8f9bd6fdd4573fe9ccee0d362b691253a3
Zen-nojen <jiyoyo1234@naver.com>
2025-02-09 20:23:54 +0900

    article 클래스 파일의 들여쓰기가 잘못된 점을 수정했습니다.

--- articleClass.js
+++ articleClass.js
@@ -1,21 +1,20 @@
-class Article {
-			  constructor(title, content, writer = "", likeCount = 0) {
-				  			  		      this.title = title;
-				  			  		      this.content = content;
-				  			  		      this.writer = writer;
-				  			  		      this._likeCount = likeCount;
-				  			  		      this.createdAt = new Date();
-				  			  		    }
+class Article {constructor(title, content, writer = "", likeCount = 0) {
+	this.title = title;
+	this.content = content;
+	this.writer = writer;
+	this._likeCount = likeCount;
+	this.createdAt = new Date();
+}
 
-			  set likeCount(count) {
-				  			  		      throw new Error("call like()");
-				  			  		    }
+set likeCount(count) {
+	throw new Error("call like()");
+}
 
-			  get likeCount() {
-				  			  		      return this._likeCount;
-				  			  		    }
+get likeCount() {
+	return this._likeCount;
+}
 
-			  like() {
-				  			  		      this._likeCount += 1;
-				  			  		    }
+like() {
+	this._likeCount += 1;
+}
 }
